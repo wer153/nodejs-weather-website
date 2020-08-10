29f788316fa148f55da34fecb73a0bdb1641fe49
Kim <77wer153@gmail.com>
2020-08-11 04:56:57 +0900

    update new weather icon; Thunder,blizzard

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,20 @@
 console.log("Client side javascript file is loaded!")
 
-
+//before user clicks button
 $("#mainText").text("From JavaScript")
 $(".hide").hide()
-
+//when user clicks button
 $("button").click((e) =>
 {
   $("#mainText").text("Loading...")
-
   e.preventDefault()
+  //call geocode api and weather cast api
   const location = $("input").val()
   fetch("/weather?address="+location).then((response) =>
   {
     response.json().then((data) =>
     {
+      //in canse of error
       $(".hide").hide()
       if(data.error)
       { $("#mainText").text(data.error) }
@@ -24,6 +25,7 @@ $("button").click((e) =>
         const temperature = data.forecast.temperature
         const feelslike = data.forecast.feelslike
 
+        // Show weather icon following the weather
         if(description.toUpperCase().includes("RAIN"))
         { $("p img").attr('src','img/umbrella.svg').show() }
         else if(description.toUpperCase().includes("SUNNY"))
@@ -32,14 +34,15 @@ $("button").click((e) =>
         { $("p img").attr('src','img/cloud.svg').show()}
         else if(description.toUpperCase().includes("CLEAR"))
         { $("p img").attr('src','img/sun.svg').show()}
-        else if(description.toUpperCase().includes("LIGHTENING"))
-        { $("p img").attr('src','img/cloud-lightning.svg').show()}
+        else if(description.toUpperCase().includes("THUNDER"))
+        { $("p img").attr('src','img/cloud-lightnBLIZZARDing.svg').show()}
+        else if(description.toUpperCase().includes("BLIZZARD"))
+        { $("p img").attr('src','img/cloud-snow.svg').show()}
         else
         { $("p img").hide() }
 
-        // Show weather icon following the weather
+        //Visualize temperature,feels like
         $(".hide").show()
-
         $("#mainText").text(data.location)
         $("#description").text(description)
         $("#progressbarTemperature").text(data.forecast.temperature).removeClass().addClass("progress-bar")
@@ -53,11 +56,11 @@ $("button").click((e) =>
         { $("#progressbarTemperature").addClass("bg-danger").css("width","85%") }
 
         $("#progressbarFeelslike").text(data.forecast.feelslike).removeClass().addClass("progress-bar")
-        if(temperature<0)
+        if(feelslike<0)
         { $("#progressbarFeelslike").addClass("bg-primary").css("width","15%") }
-        else if(temperature<15)
+        else if(feelslike<15)
         { $("#progressbarFeelslike").addClass("bg-info").css("width","40%") }
-        else if(temperature<30)
+        else if(feelslike<30)
         { $("#progressbarFeelslike").addClass("bg-warning").css("width","60%") }
         else
         { $("#progressbarFeelslike").addClass("bg-danger").css("width","85%") }
