b5ee3e51fd831692be6489dde46e4d7f30f00dac
Kim <77wer153@gmail.com>
2020-08-10 22:57:12 +0900

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ weatherForm.click((e) =>
   messageOne.text("")
   e.preventDefault()
   const location = search.val()
-  fetch("http://localhost:3000/weather?address="+location).then((response) =>
+  fetch("/weather?address="+location).then((response) =>
   {
     response.json().then((data) =>
     {
